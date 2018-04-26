01904b2d409a7fcd02df3f5de612677ba401af15
lwasscooper2846 <38434327+lwasscooper2846@users.noreply.github.com>
2018-04-26 01:04:54 +0100

    Domain entry
    domain entry set up

---
+++ src/main/java/com/liamcooper/assignment/Domain/GuestBookEntry.java
@@ -0,0 +1,20 @@
+package com.liamcooper.assignment.Domain;
+
+import javax.validation.constraints.NotEmpty;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+public class GuestBookEntry {
+    @Id
+    @GeneratedValue (strategy = GenerationType.AUTO)
+    @Column(name = "entry_id")
+    private Integer id;
+
+    @NotEmpty
+    private String user;
+
+    @NotEmpty
+    private String comment;
+}
