b31f66e96971e520f228514480f49fa772e63bf8
lwasscooper2846 <38434327+lwasscooper2846@users.noreply.github.com>
2018-04-26 01:06:18 +0100

    changes

--- src/main/java/com/liamcooper/assignment/Controller/GuestBookController.java
+++ src/main/java/com/liamcooper/assignment/Controller/GuestBookController.java
@@ -1,4 +1,14 @@
 package com.liamcooper.assignment.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class GuestBookController {
+
+    @GetMapping("/")
+    public String testMapping () {
+        return "Hello, World";
+    }
+
 }
