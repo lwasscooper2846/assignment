125c28956294fd055c54cfd5bf7a93d9001c6366
lwasscooper2846 <38434327+lwasscooper2846@users.noreply.github.com>
2018-04-26 20:25:22 +0100

    guest book entry

--- src/main/java/com/liamcooper/assignment/Domain/GuestBookEntry.java
+++ src/main/java/com/liamcooper/assignment/Domain/GuestBookEntry.java
@@ -1,11 +1,14 @@
 package com.liamcooper.assignment.Domain;
 
-import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.NotEmpty;
+import org.springframework.data.repository.CrudRepository;
 
 import javax.persistence.*;
 import java.util.List;
 
 @Entity
+@Table (name = "entries")
 public class GuestBookEntry {
     @Id
     @GeneratedValue (strategy = GenerationType.AUTO)
@@ -17,4 +20,55 @@ public class GuestBookEntry {
 
     @NotEmpty
     private String comment;
+
+    public GuestBookEntry () {
+    }
+
+    public GuestBookEntry (String user, String comment) {
+        this.user = user;
+        this.comment = comment;
+    }
+
+    public GuestBookEntry (Integer id, String user, String comment) {
+        this.id = id;
+        this.user = user;
+        this.comment = comment;
+    }
+
+    public Integer getId () {
+        return id;
+    }
+
+
+    public void setId (Integer id) {
+        this.id = id;
+    }
+
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser (String user) {
+        this.user = user;
+    }
+
+
+    public String getComment() {
+        return comment;
+    }
+
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    @Override
+    public String toString () {
+        return "GuestBookEntry{" +
+                "id=" + id +
+                ", user='" + user + '\'' +
+                ", comment='" + comment + '\'' +
+                '}';
+    }
 }
