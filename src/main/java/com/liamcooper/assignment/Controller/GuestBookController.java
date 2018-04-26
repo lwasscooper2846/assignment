62c0fdbdd5ebf6cd73cf66f5137f5bc83d9845d6
lwasscooper2846 <38434327+lwasscooper2846@users.noreply.github.com>
2018-04-26 17:18:09 +0100

    Clearing Address Space
    Adding /api to the guestbook url to give it its own space

--- src/main/java/com/liamcooper/assignment/Controller/GuestBookController.java
+++ src/main/java/com/liamcooper/assignment/Controller/GuestBookController.java
@@ -1,14 +1,46 @@
 package com.liamcooper.assignment.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.liamcooper.assignment.Domain.GuestBookEntry;
+import com.liamcooper.assignment.Service.GuestBookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+@RequestMapping ("/api")
 @RestController
 public class GuestBookController {
 
-    @GetMapping("/")
-    public String testMapping () {
-        return "Hello, World";
+    @Autowired
+    private GuestBookService guestBookService;
+
+    @GetMapping ("/comments")
+    public List<GuestBookEntry> getAllComments () {
+        return guestBookService.findAllEntries ();
     }
 
+
+    @GetMapping ("/comment/{id}")
+    public GuestBookEntry findGuestBookEntryById (@PathVariable("id") Integer id) {
+        return this.guestBookService.findGuestBookEntryById (id);
+    }
+
+    @DeleteMapping("/comment/{id}")
+    public void deleteGuestBookEntryById (@PathVariable ("id") Integer id) {
+        this.guestBookService.deleteGuestBookEntryById (id);
+    }
+
+    @GetMapping ("/user/{user}")
+    public List <GuestBookEntry> findGuestBookEntryByUser (@PathVariable ("user") String user) {
+        return this.guestBookService.findGuestBookEntryByUser (user);
+    }
+
+    @PostMapping("/add")
+    public void addComment (@RequestBody GuestBookEntry guestBookEntry) {
+        this.guestBookService.save (guestBookEntry);
+    }
+
+    @PostMapping ("/update")
+    public void updateComment (@RequestBody GuestBookEntry guestBookEntry) {
+        this.guestBookService.save (guestBookEntry);
+    }
 }
